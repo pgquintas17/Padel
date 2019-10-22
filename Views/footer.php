34d6e76fcd7ced25100f407ab4b89d50cd2a6bd3
Paula <paulaseis@hotmail.com>
2019-10-22 20:30:28 +0200

    Added template Views

---
+++ Views/footer.php
@@ -0,0 +1,80 @@
+<?php
+
+/*Archivo php footer, contiene el pie de la web, visible en todas las vistas.
+  Autores: Grupo ABP 35*/
+
+?>
+
+</body>
+<!-- Footer -->
+<footer class="pt-5 pb-4" id="contact">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-3 col-md-6 col-sm-6 mt-2 mb-4">
+        <h5 class="mb-4 font-weight-bold">CONTACTO</h5>
+        <ul class="f-address">
+          <li>
+            <div class="row">
+              <div class="col-1"><i class="fas fa-map-marker"></i></div>
+              <div class="col-10">
+                <h6 class="font-weight-bold mb-0">Dirección:</h6>
+                <p>Edificio Politécnico s/n, 32004 Ourense</p>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 mt-2 mb-4">
+          <h5 class="mb-4 font-weight-bold"><br></h5>
+        <ul class="f-address">
+          <li>
+            <div class="row">
+              <div class="col-1"><i class="far fa-envelope"></i></div>
+              <div class="col-10">
+                <h6 class="font-weight-bold mb-0">¿Alguna pregunta?</h6>
+                <p><a href="#">Support@userthemes.com</a></p>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 mt-2 mb-4">
+          <h5 class="mb-4 font-weight-bold"><br></h5>
+        <ul class="f-address">
+          <li>
+            <div class="row">
+              <div class="col-1"><i class="fas fa-phone-volume"></i></div>
+              <div class="col-10">
+                <h6 class="font-weight-bold mb-0">Teléfono:</h6>
+                <p><a href="#">+XX (0) XX XX-XXXX-XXXX</a></p>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 mt-2 mb-4">
+        <h5 class="mb-4 font-weight-bold">REDES SOCIALES</h5>
+        <ul class="social-pet mt-4">
+          <li><a href="#" title="facebook"><i class="fab fa-facebook-f"></i></a></li>
+          <li><a href="#" title="twitter"><i class="fab fa-twitter"></i></a></li>
+          <li><a href="#" title="google-plus"><i class="fab fa-google-plus-g"></i></a></li>
+          <li><a href="#" title="instagram"><i class="fab fa-instagram"></i></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
+<!-- Copyright -->
+<section class="copyright">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 ">
+        <div class="text-center text-white">
+          &copy; 2019 Grupo 35 ABP. All Rights Reserved.
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+</html>
