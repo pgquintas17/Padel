fd3439a4f931efe613ce3b78c95ec06d3f1eb690
Paula <paulaseis@hotmail.com>
2019-11-01 12:30:23 +0100

    added set model (not finished)

---
+++ Models/SET_MODEL.php
@@ -0,0 +1,21 @@
+
+<?php
+    
+class SET_MODEL
+{
+ 	var $n_set; 
+    var $punt_pareja1;  
+	var $punt_pareja2; 
+	
+
+ 	function __construct($id_pista,$estado){
+        $this->id_pista = $id_pista;
+        $this->estado = $estado;
+		include_once '../Models/BdAdmin.php'; 
+		$this->mysqli = ConectarBD();  
+	}
+
+
+}
+
+?>
