231e5bc655c0bbadc2de35ba9435e622a02ff900
Paula <paulaseis@hotmail.com>
2019-11-01 12:30:03 +0100

    template for functions

--- plantilla funciones.php
+++ plantilla funciones.php
@@ -64,14 +64,5 @@
      else
          return false;*/
  }
- static function nivelPermiso($nivelRequerido){
-     $login = $_SESSION['login'];
-     include '../Models/USUARIO_MODEL.php';
-     $control = new USUARIO_MODEL($login,0,0,0,0,0,0,0,$nivelRequerido);
-     $resultado = $control->comprobarNivelAcceso();
-     if($resultado == true)
-         return true;
-     else
-         return false;
- }
+ 
  }
