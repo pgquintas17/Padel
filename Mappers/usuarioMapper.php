a46666dd576f5cff9a4f65d6a71a44a38ceae96b
Paula <paulaseis@hotmail.com>
2019-11-02 13:23:18 +0100

    added usuarioMapper

---
+++ Mappers/usuarioMapper.php
@@ -0,0 +1,203 @@
+<?php
+
+require_once('Models/usuarioModel.php');
+
+    class UsuarioMapper{
+
+        var $bd;
+
+        function __construct(){
+            include_once 'Models/BdAdmin.php'; 
+		    $this->mysqli = ConectarBD();
+        }
+
+
+        function ADD($usuario){
+
+            $login = $usuario->getLogin();
+            $nombre = $usuario->getNombre();
+            $password = $usuario->getPassword();
+            $fechaNac = $usuario->getFechaNac();
+            $telefono = $usuario->getTelefono();
+            $email = $usuario->getEmail();
+            $genero = $usuario->getGenero();
+            $permiso = $usuario->getPermiso();
+
+            if (($usuario->getLogin() <> '')){ 
+    
+                $sql = "SELECT * FROM USUARIO WHERE (login = '$login')";
+    
+                if (!$result = $this->mysqli->query($sql)) 
+                    return 'No se ha podido conectar con la base de datos'; 
+                else { 
+                    if ($result->num_rows == 0){ 
+                    
+                        $sql = "INSERT INTO USUARIO (
+                                login,
+                                nombre,
+                                password,
+                                fecha_nac,
+                                telefono,
+                                email,
+                                genero,
+                                permiso
+                            )
+                            VALUES (
+                                '$login()',
+                                '$nombre',
+                                '$password',
+                                '$fechaNac',
+                                '$telefono',
+                                '$email',
+                                '$genero',
+                                '$permiso'
+                            )";
+    
+                        if (!$this->mysqli->query($sql)) 
+                            return 'Error en la inserción';
+                        else 
+                            return 'Inserción realizada con éxito'; 
+                    }
+                    else 
+                        return 'Ya existe en la base de datos'; 
+                }
+            }
+            else 
+                return 'Introduzca un valor'; 
+        }
+
+
+
+        function EDIT($usuario) {
+
+            $login = $usuario->getLogin();
+            $nombre = $usuario->getNombre();
+            $password = $usuario->getPassword();
+            $fechaNac = $usuario->getFechaNac();
+            $telefono = $usuario->getTelefono();
+            $email = $usuario->getEmail();
+            $genero = $usuario->getGenero();
+	
+            $sql = "SELECT * FROM USUARIO  WHERE (login = '$login')";
+            $result = $this->mysqli->query($sql);
+        
+            if ($result->num_rows == 1) {	
+    
+                $sql = "UPDATE USUARIO  SET
+                            nombre = '$nombre',
+                            password = '$password',
+                            fecha_nac = '$fechaNac',
+                            telefono = '$telefono',
+                            email = '$email',
+                            genero = '$genero'
+    
+                        WHERE ( login = '$login')";
+    
+                if (!($resultado = $this->mysqli->query($sql)))
+                    return 'Error en la modificación';
+                else
+                    return 'Modificado correctamente';
+            }
+            else 
+                return 'No existe en la base de datos';
+        }
+
+
+        function DELETE() {	
+
+            $login = $usuario->getLogin();
+		
+            $sql = "SELECT * FROM USUARIO  WHERE (login = '$login') ";    
+            $result = $this->mysqli->query($sql);
+    
+            if (!$result)
+                return 'No se ha podido conectar con la base de datos';
+                
+            if ($result->num_rows == 1) {
+                    
+                $sql = "DELETE FROM  USUARIO WHERE (login = '$login')";   
+                $this->mysqli->query($sql);
+                
+                return "Borrado correctamente";
+            } 
+            else
+                return "No existe";
+        }
+
+
+        function mostrarTodos() {
+		
+            $sql = "select 
+                        login,
+                        nombre,
+                        password,
+                        fecha_nac,
+                        telefono,
+                        email,
+                        genero,
+                        permiso 
+                    from USUARIO";
+    
+            if (!($resultado = $this->mysqli->query($sql)))
+                return 'Error en la consulta sobre la base de datos';
+            else
+                return $resultado;
+        }
+
+
+        function consultarDatos($usuario) {	
+
+            $login = $usuario->getLogin();
+
+            $sql = "SELECT * FROM USUARIO WHERE (login = '$login')";
+                
+            if (!($resultado = $this->mysqli->query($sql)))
+                return 'No existe en la base de datos'; 
+            else{ 
+                $result = $resultado->fetch_array();
+                return $result;
+            }
+        }
+
+
+
+        function Login($usuario){
+
+            $login = $usuario->getLogin();
+
+            $sql = "SELECT * FROM USUARIO
+                WHERE  (login = '$login')";
+            
+            $resultado = $this->mysqli->query($sql);
+            
+            if ($resultado->num_rows == 0)
+                return 'El login no existe';
+            else{
+                $tupla = $resultado->fetch_array();
+                if ($tupla['password'] == $usuario->getPassword())
+                    return true;
+                else
+                    return 'La contraseña para este usuario no es correcta';
+            }
+        }
+    
+    
+        function comprobarNivelAcceso($usuario,$nivelAcceso){
+
+            $login = $usuario->getLogin();
+            
+            $sql = "SELECT * FROM USUARIO WHERE ((login = '$login') && (permiso = '$nivelAcceso'))";
+            
+            if (!($resultado = $this->mysqli->query($sql)))
+                return false;
+    
+            if ($resultado->num_rows == 1)
+                return true;
+            else 
+                return false;
+        }
+
+
+    }
+
+?>
