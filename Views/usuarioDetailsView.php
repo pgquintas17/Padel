9db33f937a6ccc336d3a4f7da8dda6deeca42976
Paula <paulaseis@hotmail.com>
2019-11-02 20:46:37 +0100

    finished usuarioDetailsView

--- Views/usuarioDetailsView.php
+++ Views/usuarioDetailsView.php
@@ -10,7 +10,6 @@
         private $datos;
 
         function __construct($msg=null, $errs=null, $usuario=null, $datos=null) {
-            $this->usuario = $usuario;
             $this->msg = $msg;
             $this->errs = $errs;
             parent::__construct($this->usuario);
@@ -20,48 +19,53 @@
         function _render() { 
             ?>
             <!-- Jumbotron -->
-            <div  id="espacio_info" class="jumbotron"> 
+            <div  id="espacio_info" class="jumbotron">
+            <h3>Usuario: <?php echo $this->datos['1']; ?></h3><br>
+                <div class="row justify-content-md-center">
+                    <div class="col-md-auto"> 
 
-                <!-- Tabla datos usuario -->
-                <table class="table table-hover table-bordered" id="tablas" style="border-radius: 25px;">
-                    <tr>
-                        <th class="bg-dark" scope="col">Login</th>
-                        <td><?php echo $this->datos['0']; ?></td>
-                    </tr>
-                    <tr>    
-                        <th class="bg-dark" scope="col">Nombre</th>
-                        <td><?php echo $this->datos['1']; ?></td>
-                    </tr>
-                    <tr>
-                        <th class="bg-dark" scope="col">Fecha de Nacimiento</th>
-                        <td><?php echo $this->datos['2']; ?></td>
-                    </tr>
-                    <tr>
-                        <th class="bg-dark" scope="col">Teléfono</th>
-                        <td><?php echo $this->datos['3']; ?></td>
-                    </tr>
-                    <tr>
-                        <th class="bg-dark" scope="col">Email</th>
-                        <td><?php echo $this->datos['4']; ?></td>
-                    </tr>
-                    <tr>
-                        <th class="bg-dark" scope="col">Genero</th>
-                        <td><?php echo $this->datos['5']; ?></td>
-                    </tr>
-                    <tr>
-                        <th class="bg-dark" scope="col">Permiso</th>
-                        <td>
-                            <?php 
-                                if($this->datos['6'] == 0)
-                                    echo "Deportista";
-                                if($this->datos['6'] == 1)
-                                    echo "Entrenador";
-                                if($this->datos['6'] == 2)
-                                    echo "Administrador"; 
-                            ?>
-                        </td>
-                    </tr>
-                </table>
+                        <!-- Tabla datos usuario -->
+                        <table class="table table-hover table-bordered" id="tablas" style="border-radius: 25px;">
+                            <tr>
+                            <th class="bg-dark text-white">Login</th>
+                                <td><?php echo $this->datos['0']; ?></td>
+                            </tr>
+                            <tr>    
+                                <th class="bg-dark text-white">Nombre</th>
+                                <td><?php echo $this->datos['1']; ?></td>
+                            </tr>
+                            <tr>
+                                <th class="bg-dark text-white">Fecha de Nacimiento</th>
+                                <td><?php echo $this->datos['2']; ?></td>
+                            </tr>
+                            <tr>
+                                <th class="bg-dark text-white">Teléfono</th>
+                                <td><?php echo $this->datos['3']; ?></td>
+                            </tr>
+                            <tr>
+                                <th class="bg-dark text-white">Email</th>
+                                <td><?php echo $this->datos['4']; ?></td>
+                            </tr>
+                            <tr>
+                                <th class="bg-dark text-white">Genero</th>
+                                <td><?php echo $this->datos['5']; ?></td>
+                            </tr>
+                            <tr>
+                                <th class="bg-dark text-white">Permiso</th>
+                                <td>
+                                    <?php 
+                                        if($this->datos['6'] == 0)
+                                            echo "Deportista";
+                                        if($this->datos['6'] == 1)
+                                            echo "Entrenador";
+                                        if($this->datos['6'] == 2)
+                                            echo "Administrador"; 
+                                    ?>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
             </div>
 
         <?php
