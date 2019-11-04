89a858a7f833ef8e2d0ba9384543cc60af510724
Paula <paulaseis@hotmail.com>
2019-11-04 17:43:31 +0100

    added links to footer

--- Views/baseView.php
+++ Views/baseView.php
@@ -104,7 +104,7 @@
                                 <div class="col-1"><i class="fas fa-map-marker"></i></div>
                                 <div class="col-10">
                                     <h6 class="font-weight-bold mb-0">Dirección:</h6>
-                                    <p>Edificio Politécnico s/n, 32004 Ourense</p>
+                                    <p><a href="https://goo.gl/maps/V6c8PxYhuLHUsHWy7">Edificio Politécnico s/n, 32004 Ourense</a></p>
                                 </div>
                                 </div>
                             </li>
@@ -118,7 +118,7 @@
                                 <div class="col-1"><i class="far fa-envelope"></i></div>
                                 <div class="col-10">
                                     <h6 class="font-weight-bold mb-0">¿Alguna pregunta?</h6>
-                                    <p><a href="#">Support@userthemes.com</a></p>
+                                    <p><a href="mailto:info@esei.uvigo.es">info@esei.uvigo.es</a></p>
                                 </div>
                                 </div>
                             </li>
@@ -132,7 +132,7 @@
                                 <div class="col-1"><i class="fas fa-phone-volume"></i></div>
                                 <div class="col-10">
                                     <h6 class="font-weight-bold mb-0">Teléfono:</h6>
-                                    <p><a href="#">+XX (0) XX XX-XXXX-XXXX</a></p>
+                                    <p><a href="tel:+34988387000">+34 988 387 000</a></p>
                                 </div>
                                 </div>
                             </li>
@@ -141,10 +141,10 @@
                         <div class="col-lg-3 col-md-6 col-sm-6 mt-2 mb-4">
                             <h5 class="mb-4 font-weight-bold">REDES SOCIALES</h5>
                             <ul class="social-pet mt-4">
-                            <li><a href="#" title="facebook"><i class="fab fa-facebook-f"></i></a></li>
-                            <li><a href="#" title="twitter"><i class="fab fa-twitter"></i></a></li>
-                            <li><a href="#" title="google-plus"><i class="fab fa-google-plus-g"></i></a></li>
-                            <li><a href="#" title="instagram"><i class="fab fa-instagram"></i></a></li>
+                            <li><a href="https://www.facebook.com/eseiuvigo" title="facebook"><i class="fab fa-facebook-f"></i></a></li>
+                            <li><a href="https://twitter.com/eseiuvigo" title="twitter"><i class="fab fa-twitter"></i></a></li>
+                            <li><a href="https://www.youtube.com/user/uvigo" title="youtube"><i class="fab fa-youtube"></i></a></li>
+                            <li><a href="https://es.linkedin.com/school/uvigo/" title="linkedin"><i class="fab fa-linkedin-in"></i></a></li>
                             </ul>
                         </div>
                         </div>
