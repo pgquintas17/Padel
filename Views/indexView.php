78a7e5b0f2623985d3307329f7c0942e073a5166
Paula <paulaseis@hotmail.com>
2019-11-05 21:17:20 +0100

    added diferent views for partidosPromocionados in index

--- Views/indexView.php
+++ Views/indexView.php
@@ -2,19 +2,23 @@
 
 require_once('Views/baseView.php');
 require_once('Views/mensajeView.php');
+require_once('Mappers/partidoMapper.php');
+require_once('Models/usuarioModel.php');
+require_once('Services/Utils.php');
 
 class IndexView extends baseView {
 
     private $usuario;
     private $msg;
     private $errs;
-    private $partidosPromocionados;
+    private $partidos;
 
-    function __construct($msg=null, $errs=null, $usuario=null, $partidosPromocionados=null) {
+    function __construct($msg=null, $errs=null, $usuario=null, $fila=null, $partidos=null) {
         $this->msg = $msg;
         $this->errs = $errs;
         parent::__construct($this->usuario);
-        $this->partidosPromocionados = $partidosPromocionados;
+        $this->fila = array('id_partido','resultado','hora','fecha','promocion','login1','login2','login3','login4','id_reserva');
+        $this->partidos = $partidos;
     }
 
     function _render() { 
@@ -29,11 +33,88 @@ class IndexView extends baseView {
             <h1>Noticias</h1>
             <!-- Example row of columns -->
             <div class="row">
-                <div class="col-lg-4">
-                    <h2>¡Hazte socio!</h2>
-                    <p id="centrar" class="text-danger">¡Plazas limitadas!</p>
-                    <p id="justificar">Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna. </p>
-                    <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+                <div id="partidos" class="col-lg-4">
+                    <?php
+                        if(Utils::conectado()){
+                         if($this->partidos != null) {
+                            if(($_SESSION['Usuario'])->getPermiso() == 0){
+                            ?>
+                            <h5>¡Apúntate a nuestros partidos!</h5>
+                            <p id="justificar">
+                            <?php
+                            while($this->fila = ($this->partidos)->fetch_assoc()) {
+                                $id = $this->fila['id_partido'];
+                                $numPlazas = (new PartidoMapper())->getNumPlazasLibres($id);
+                                if($numPlazas != 0){
+                                ?>
+                                <li class="list-group-item">
+                                    <strong><?php echo $this->fila['fecha']; ?></strong><br>
+                                    <p>Se jugará a las <?php echo $this->fila['hora']; ?><br>
+                                    ¡Quedan <?php echo $numPlazas; ?> plazas libres!</p>
+                                    <p><a class="btn btn-dark" href="#" role="button">Apuntarse</a></p>
+                                </li>
+                                <?php
+                                }
+                            }
+                            ?>
+                            </p>
+                            <?php
+                            }
+                            else{
+                                ?>
+                            <h5>Partidos abiertos</h5>
+                            <p id="justificar">
+                            <?php
+                            while($this->fila = ($this->partidos)->fetch_assoc()) {
+                                $id = $this->fila['id_partido'];
+                                $numPlazas = (new PartidoMapper())->getNumPlazasLibres($id);
+                                ?>
+                                <li class="list-group-item">
+                                    <strong><?php echo $this->fila['fecha']; ?></strong><br>
+                                    <p>Se jugará a las <?php echo $this->fila['hora']; ?><br>
+                                    Quedan <?php echo $numPlazas; ?> plazas libres</p>
+                                    <?php
+                                    if($numPlazas == 0){
+                                        ?>
+                                    <p><a class="btn btn-dark" href="#" role="button">Cerrar partido</a></p>
+                                </li>
+                                <?php
+                                }
+                            }
+                            ?>
+                            </p>
+                            <?php
+                            }
+                        }
+                        else{
+                            ?>
+                            <h5>No hay partidos disponibles en este momento.</h5>
+                        <?php
+                        }
+                    }
+                    else{
+                        ?>
+                        <h5>¡Únete al club para poder apuntarte a los partidos!</h5>
+                            <p id="justificar">
+                            <?php
+                            while($this->fila = ($this->partidos)->fetch_assoc()) {
+                                $id = $this->fila['id_partido'];
+                                $numPlazas = (new PartidoMapper())->getNumPlazasLibres($id);
+                                if($numPlazas != 0){
+                                ?>
+                                <li class="list-group-item">
+                                    <strong><?php echo $this->fila['fecha']; ?></strong><br>
+                                    <p>Se jugará a las <?php echo $this->fila['hora']; ?><br>
+                                    ¡Quedan <?php echo $numPlazas; ?> plazas libres!</p>
+                                </li>
+                                <?php
+                                }
+                            }
+                            ?>
+                            </p>
+                    <?php
+                    }
+                    ?>
                 </div>
                 <div class="col-lg-4">
                     <h2>Nuevos campeonatos</h2>
