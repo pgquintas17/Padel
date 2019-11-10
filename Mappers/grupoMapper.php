72de385f523d6227c8f40335f442593053b2b8b1
Paula <paulaseis@hotmail.com>
2019-11-10 13:31:24 +0100

    added mapper/model grupo

---
+++ Mappers/grupoMapper.php
@@ -0,0 +1,85 @@
+<?php
+
+require_once('Models/horaModel.php');
+
+    class GrupoMapper{
+
+        var $bd;
+
+        function __construct(){
+            include_once 'Models/BdAdmin.php'; 
+		    $this->mysqli = ConectarBD();
+        }
+
+
+        function ADD($grupo){
+
+            $id_catcamp = $grupo->getIdCatCamp();
+            $n_parejas = $grupo->getNumParejas();
+
+            $sql = "INSERT INTO GRUPO (
+                        id_catcamp,
+                        n_parejas
+                    )
+                    VALUES (
+                        '$id_catcamp',
+                        '$n_parejas'
+                    )";
+            if (!$this->mysqli->query($sql))
+                return 'Error en la inserción';
+            else 
+                return 'Inserción realizada con éxito'; 
+        }
+    
+            
+        function DELETE($grupo) {
+            
+            $id_grupo = $grupo->getId();
+            
+            $sql = "SELECT * FROM GRUPO  WHERE (id_grupo = '$id_grupo') ";
+            $result = $this->mysqli->query($sql);
+    
+            if (!$result)
+                return 'No se ha podido conectar con la base de datos';
+                
+            if ($result->num_rows == 1) {
+                    
+                $sql = "DELETE FROM GRUPO WHERE (id_grupo = '$id_grupo')";
+                $this->mysqli->query($sql);
+                    
+                return "Borrado correctamente";
+            } 
+            else
+                return "No existe";
+        } 
+    
+    
+        function mostrarTodos() {
+            
+            $sql = "SELECT id_grupo, id_catcamp, n_parejas FROM GRUPO";
+    
+            if (!($resultado = $this->mysqli->query($sql)))
+                return 'Error en la consulta sobre la base de datos';
+            else
+                return $resultado;
+        }
+    
+    
+        function consultarDatos($grupo) {
+
+            $id_grupo = $grupo->getId();
+    
+            $sql = "SELECT * FROM GRUPO WHERE (id_grupo = '$id_grupo')";
+                
+            if (!($resultado = $this->mysqli->query($sql)))
+                return 'No existe en la base de datos'; 
+            else{ 
+                $result = $resultado->fetch_array(MYSQLI_NUM);
+                return $result;
+            }
+        }
+        
+
+    }
+
+?>
--- Models/grupoModel.php
+++ Models/grupoModel.php
@@ -2,121 +2,44 @@
     
 class GrupoModel {
 
- 	var $id_grupo; 
-    var $id_catcamp;
-	var $bd; 
+ 	private $id_grupo; 
+	private $id_catcamp;
+	private $n_parejas; 
 	
 
- 	function __construct($id_grupo,$id_catcamp){
+ 	function __construct($id_grupo=null,$id_catcamp=null,$n_parejas=null){
         $this->id_grupo = $id_grupo;
-        $this->id_catcamp = $id_catcamp;
-		include_once '../Models/BdAdmin.php'; 
-		$this->mysqli = ConectarBD();  
-	}
-
-		
-	function ADD(){
-
-		$sql = "INSERT INTO GRUPO (
-                    id_catcamp
-				)
-				VALUES (
-                    '$this->id_catcamp'
-				)";
-		if (!$this->mysqli->query($sql))
-			return 'Error en la inserción';
-		else 
-			return 'Inserción realizada con éxito'; 
-	}
-	
-	
-	function EDIT() {
-	
-    	$sql = "SELECT * FROM GRUPO  WHERE (id_grupo = '$this->id_grupo') ";
-   		$result = $this->mysqli->query($sql);
-    
-    	if ($result->num_rows == 1) {	
-
-			$sql = "UPDATE GRUPO  SET
-                        id_catcamp = '$this->id_catcamp'
-
-					WHERE ( id_grupo = '$this->id_grupo')";
-
-        	if (!($resultado = $this->mysqli->query($sql)))
-				return 'Error en la modificación';
-		else 
-			return 'Modificado correctamente';
-    	}
-		else
-			return 'No existe en la base de datos';
-	}
-
-		
-	function DELETE() {	
-		
-		$sql = "SELECT * FROM GRUPO  WHERE (id_grupo = '$this->id_grupo') ";
-		$result = $this->mysqli->query($sql);
-
-		if (!$result)
-    		return 'No se ha podido conectar con la base de datos';
-		    
-		if ($result->num_rows == 1) {
-		    	
-		    $sql = "DELETE FROM GRUPO WHERE (id_grupo = '$this->id_grupo')";
-		    $this->mysqli->query($sql);
-		        
-		    return "Borrado correctamente";
-		} 
-		else
-		    return "No existe";
-	} 
-
-
-	function mostrarTodos() {
-		
-		$sql = "SELECT * FROM RESERVA";
-
-    	if (!($resultado = $this->mysqli->query($sql)))
-			return 'Error en la consulta sobre la base de datos';
-    	else
-			return $resultado;
+		$this->id_catcamp = $id_catcamp;
+		$this->n_parejas = $n_parejas;  
 	}
 
 
-	function consultarDatos() {
+	//getters
 
-		$sql = "SELECT * FROM RESERVA WHERE (id_reserva = '$this->id_reserva')";
-		    
-		if (!($resultado = $this->mysqli->query($sql)))
-			return 'No existe en la base de datos'; 
-		else{ 
-			$result = $resultado->fetch_array();
-			return $result;
-		}
+	public function getId(){
+		return $this->id_grupo;
 	}
-	
-	
-	function crearFiltros($filtros) {
-		$toret = "( ";
-		int n = 
-		foreach($filtros as $filtro) {
-			switch($filtro) {
-				case "id_catcamp":
-					$toret .= "(id_catcamp LIKE '%$this->id_catcamp%')";
-					break;
-			}
-			$toret .= " && ";
-		}
-		$toret = chop($toret," && ");
-		$toret .= " )";
 
-		$sql = "SELECT * FROM GRUPO WHERE " . $toret;
+	public function getIdCatCamp(){
+		return $this->id_catcamp;
+	}
 
-    	if (!($resultado = $this->mysqli->query($sql))) {
-			return 'Error en la consulta sobre la base de datos';
-		}
-    	else 
-			return $resultado;
+	public function getNumParejas(){
+		return $this->n_parejas;
+	}
+
+	//setters
+
+	public function setId($id_grupo){
+		$this->id_grupo = $id_grupo;
+	}
+
+	public function setIdCatCamp($id_catcamp){
+		$this->id_catcamp = $id_catcamp;
+	}
+
+	public function setNumParejas($n_parejas){
+		$this->n_parejas = $n_parejas;
 	}
 
 
