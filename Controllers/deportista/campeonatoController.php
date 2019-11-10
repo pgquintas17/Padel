ea47b0e0eba3960cebdb17cef4ceefc2a5ae88c2
Paula <paulaseis@hotmail.com>
2019-11-10 19:49:20 +0100

    added generic view for Campeonatos

---
+++ Controllers/deportista/campeonatoController.php
@@ -0,0 +1,81 @@
+<?php	
+
+	require_once('Services/sessionMensajes.php');
+    require_once("Services/validarExcepciones.php");
+
+	class CampeonatoController {
+
+		function __construct() {
+
+			if(isset($_REQUEST["action"])) {
+				switch($_REQUEST["action"]) {
+
+                    case 'inscripcion': 
+                        echo "add pareja";
+						/* if ($_POST){
+							try {
+								require_once('Models/usuarioModel.php');
+								$usuario = new UsuarioModel($_POST["inputLogin"],$_POST["inputNombre"],$_POST["inputLogin"],$_POST["inputFechaNac"],$_POST["inputTelefono"],$_POST["inputEmail"],$_POST["inputGenero"],$_REQUEST["inputPermiso"]);
+								$errores =  $usuario->validarRegistro();
+								require_once('Mappers/usuarioMapper.php');
+                            	$usuarioMapper = new UsuarioMapper();
+								$respuesta = $usuarioMapper->ADD($usuario);
+
+								SessionMessage::setMessage($respuesta);
+								header('Location: index.php?controller=adminUsuarios');
+							}
+							catch (ValidationException $e){
+								SessionMessage::setErrores($e->getErrores());
+								SessionMessage::setMessage($e->getMessage());
+								header('Location: index.php?controller=adminUsuarios&action=ADD');
+							}
+						}else{
+							require_once('Views/usuarioADDView.php');
+							(new UsuarioADDView(SessionMessage::getMessage(),SessionMessage::getErrores()))->render();
+						} */
+						break;
+						
+
+                    case 'borrar': 
+                        echo "delete pareja";
+						/* require_once('Models/usuarioModel.php');
+						$usuario = new UsuarioModel();
+						$usuario->setLogin($_REQUEST['username']);
+						require_once('Mappers/usuarioMapper.php');
+						$usuarioMapper = new UsuarioMapper();
+						$respuesta = $usuarioMapper->DELETE($usuario); 
+						echo "Usuario Eliminado"; 
+						header('Location: index.php?controller=adminUsuarios'); */
+						break;
+						
+
+                    case 'DETAILS': 
+                        echo "details";
+						/* require_once('Models/usuarioModel.php');
+						$usuario = new UsuarioModel();
+						$usuario->setLogin($_REQUEST['username']);
+						require_once('Mappers/usuarioMapper.php');
+						$usuarioMapper = new UsuarioMapper();
+						$datos = $usuarioMapper->consultarDatos($usuario);
+						require_once('Views/usuarioDetailsView.php');
+						(new UsuarioDetailsView('','','',$datos))->render(); */
+						break;
+
+
+					default: 
+						echo "hey, estoy viniendo aquí";
+						header('Location: index.php?controller=adminUsuarios');
+						break;
+
+				}
+			} else { //mostrar todos los elementos
+				require_once('Mappers/campeonatoMapper.php');
+				$campeonatoMapper = new CampeonatoMapper();
+				$listaCampeonatos = $campeonatoMapper->mostrarTodos(); 
+				require_once('Views/campeonatoView.php');
+				(new CampeonatoView(SessionMessage::getMessage(), SessionMessage::getErrores(),'','',$listaCampeonatos))->render();
+			}
+		}
+	}
+
+ ?>
---
+++ Views/campeonatoView.php
@@ -0,0 +1,118 @@
+<?php
+
+    require_once('Views/baseView.php');
+    require_once('Models/usuarioModel.php');
+    require_once('Views/mensajeView.php');
+    require_once('Models/campeonatoModel.php');
+
+    class CampeonatoView extends baseView {
+
+        private $usuario;
+        private $msg;
+        private $errs;
+        private $fila;
+        private $listaCampeonatos;
+
+        function __construct($msg=null, $errs=null, $usuario=null, $fila=null, $listaCampeonatos=null) {
+            $this->msg = $msg;
+            $this->errs = $errs;
+            parent::__construct($this->usuario);
+            $this->fila = array('id_campeonato','nombre','fecha_inicio','fecha_fin','fecha_inicio_inscripciones','fecha_fin_inscripciones');
+            $this->listaCampeonatos = $listaCampeonatos;
+        }
+
+        function _render() { 
+?>
+
+        <!-- ESTA ES LA VISTA DEL MENSAJE Y DE LOS ERRORES -->
+        <?php (new MSGView($this->msg, $this->errs))->render(); ?>
+        <!-- ///////////////////////////////////////////// -->
+
+        <!-- Jumbotron -->
+        <div  id="espacio_info" class="jumbotron">
+            <h1>Campeonatos</h1><br>
+            
+            <?php
+            if(Utils::nivelPermiso(2)){
+            ?>
+
+            <div class="row justify-content-md-center">
+                <a class="bg-ligth text-dark" href='/index.php?controller=adminCampeonatos&action=ADD'><i class="fas fa-plus-circle fa-2x"></i></a>
+            </div><br>
+            
+            <?php
+            }
+            ?>
+        
+
+        <!-- Tabla campeonatos -->
+        <div id="tablas">
+            <table class="table table-hover table-bordered" style="border-radius: 25px;">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Fecha inicio</th>
+                        <th scope="col">Fecha fin</th>
+                        <th scope="col1">Periodo inscripciones</th>
+                        <th scope="col1"></th>
+                    </tr>
+                </thead>
+                <tbody>
+            <?php
+
+                    while($this->fila = ($this->listaCampeonatos)->fetch_assoc()) {
+                        $idcampeonato = $this->fila['id_campeonato'];          
+                        $url = "/index.php?controller=campeonato&action=DETAILS&idCampeonato=". $idcampeonato;
+
+                        $hoy = date('Y-m-d H:i:s');
+                        if($this->fila['fecha_inicio_inscripciones'] <= $hoy && $this->fila['fecha_fin_inscripciones'] >= $hoy){ // EN PERIODO DE INSCRIPCION
+                        ?>
+                        <tr class='table-light clickeable-row' onclick="window.location.assign('<?php echo $url ?>');" style="cursor:pointer;">
+                            <td><?php echo $this->fila['nombre']; ?></td>
+                            <td><?php echo date('d-m-Y H:i',strtotime($this->fila['fecha_inicio'])); ?></td>
+                            <td><?php echo date('d-m-Y H:i',strtotime($this->fila['fecha_fin'])); ?></td>
+                            <td><?php echo date('d-m-Y H:i',strtotime($this->fila['fecha_inicio_inscripciones'])); ?> hasta <?php echo date('d-m-Y H:i:s',strtotime($this->fila['fecha_fin_inscripciones'])); ?></td>
+                            <td>Crear Grupos</td>
+                        </tr>
+
+                        <?php
+                        }
+
+                        if($this->fila['fecha_inicio'] <= $hoy && $this->fila['fecha_fin'] > $hoy){ // EN CURSO
+                        ?>
+                        <tr class='table-warning clickeable-row' onclick="window.location.assign('<?php echo $url ?>');" style="cursor:pointer;">
+                            <td><?php echo $this->fila['nombre']; ?></td>
+                            <td><?php echo date('d-m-Y H:i',strtotime($this->fila['fecha_inicio'])); ?></td>
+                            <td><?php echo date('d-m-Y H:i',strtotime($this->fila['fecha_fin'])); ?></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+
+                        <?php
+                        }
+
+                        if($this->fila['fecha_fin'] < $hoy){ // FINALIZADOS
+                            ?>
+                            <tr class='table-secondary clickeable-row' onclick="window.location.assign('<?php echo $url ?>');" style="cursor:pointer;">
+                                <td><?php echo $this->fila['nombre']; ?></td>
+                                <td><?php echo date('d-m-Y H:i',strtotime($this->fila['fecha_inicio'])); ?></td>
+                            <td><?php echo date('d-m-Y H:i',strtotime($this->fila['fecha_fin'])); ?></td>
+                            <td></td>
+                            <td>Ver clasificación</td>
+                            </tr>
+    
+                            <?php
+                            }           
+                    }
+            
+            ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+<?php
+        }
+    }
+    
+?>
