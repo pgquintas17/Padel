001183e984f93d5a5dc0166b75bf20799afba4a2
Paula <paulaseis@hotmail.com>
2019-11-14 17:40:32 +0100

    added function GenerarResultado (not tested)

--- Services/Utils.php
+++ Services/Utils.php
@@ -28,6 +28,59 @@ class Utils {
 		else
 			return false;
 	}
+
+
+	static function calcularResultado($set1, $set2, $set3){
+
+		require_once('Models/setModel.php');
+
+		$puntosPareja1 = 0;
+		$puntosPareja2 = 0;
+
+		if($set1->getPareja1() > $set1->getPareja2()){
+			$puntosPareja1++;
+		}
+		else{
+			$puntosPareja2++;
+		}
+
+		if($set2->getPareja1() > $set2->getPareja2()){
+			$puntosPareja1++;
+		}
+		else{
+			$puntosPareja2++;
+		}
+
+		if(($puntosPareja1 == 2 || $puntosPareja2 == 2) && $set3 !=null){
+			return false;
+		}
+		else{
+			if($puntosPareja1 > $puntosPareja2){
+				
+				$resultado = array("puntos1"=>$puntosPareja1, "puntos2"=>$puntosPareja2, "ganador"=>1);
+				return $resultado;
+			}
+			else{
+				
+				$resultado = array("puntos1"=>$puntosPareja1, "puntos2"=>$puntosPareja2, "ganador"=>2);
+				return $resultado;
+			}
+		}
+
+		if($set3->getPareja1() > $set3->getPareja2()){
+			$puntosPareja1++;
+
+			$resultado = array("puntos1"=>$puntosPareja1, "puntos2"=>$puntosPareja2, "ganador"=>1);
+			return $resultado;
+		}
+		else{
+			$puntosPareja2++;
+
+			$resultado = array("puntos1"=>$puntosPareja1, "puntos2"=>$puntosPareja2, "ganador"=>2);
+			return $resultado;
+		}
+
+	}
     
 }
 ?>
