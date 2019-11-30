35edc9020d28fd933603cf52c1d91ee3fce1ecb7
Paula <paulaseis@hotmail.com>
2019-11-30 19:02:47 +0100

    added EnfrentamientoPropuesta

---
+++ Views/campeonato/enfrentamientoPropuestaView.php
@@ -0,0 +1,124 @@
+<?php
+
+    require_once('Views/baseView.php');
+    require_once('Models/usuarioModel.php');
+    require_once('Views/mensajeView.php');
+    require_once('Models/reservaModel.php');
+    require_once('Mappers/reservaMapper.php');
+    require_once('Mappers/pistaMapper.php');
+    
+    class EnfrentamientoPropuestaView extends baseView {
+
+        private $usuario;
+        private $msg;
+        private $errs;
+        private $filaHoras;
+        private $listaHoras;
+        private $fecha;
+        private $datos;
+
+        function __construct($msg=null, $errs=null, $usuario=null, $filaHoras=null, $listaHoras=null, $fecha=null, $datos=null) {
+            $this->msg = $msg;
+            $this->errs = $errs;
+            parent::__construct($this->usuario);
+            $this->filaHoras = array('id','hora');
+            $this->listaHoras = $listaHoras;
+            if($fecha == null){
+                $hoy = date('Y-m-d');
+                $this->fecha = $hoy;
+            }
+            else{
+                $this->fecha = $fecha;
+            }
+            $this->datos = $datos;
+        }
+
+        function _render() { 
+            ?>
+
+            <!-- ESTA ES LA VISTA DEL MENSAJE Y DE LOS ERRORES -->
+            <?php (new MSGView($this->msg, $this->errs))->render(); ?>
+            <!-- ///////////////////////////////////////////// -->
+            
+            <!-- Jumbotron -->
+            <div  id="espacio_info" class="jumbotron">
+            <br><h3>Proponer fecha</h3><br>
+
+            <div class="row justify-content-md-center">
+                <div class="col-2" style="align-self: center;">
+                    <a class="bg-ligth text-dark" href='/index.php?controller=campeonatos'><i class="fas fa-arrow-circle-left fa-2x"></i></a>
+                </div>
+            <div class="col-6">
+                <p style="text-align: right";><strong><?php echo "Campeonato, categoría, grupo" ;?></strong><br>
+                <strong>Parejas:</strong><?php echo "Pareja 1 VS. Pareja 2" ;?><br></p>
+            </div>
+            </div>
+
+                <div class="row">
+
+                    <div class="col-lg-2"></div>
+                    <div class="bg-dark text-white rounded p-3 col-md-8" id="perfilform">
+
+                        <!-- Formulario datos usuario -->
+                        <form action="/" method="POST" name="formPropuestaEnfrentamiento">
+
+                        <input type="hidden" name="controller" value="enfrentamientos">
+                        <input type="hidden" name="idenfrentamiento" value="<?php echo $this->datos['0']; ?>">
+
+                            <div class="justify-content-md-center">
+                                <div class="form-group col-md-6">
+                                    <label for="fecha"><strong>Día</strong></label>
+                                    <input type="date" class="form-control" name="fecha" value="<?php echo $this->fecha; ?>">
+                                </div>
+                            </div>
+                                <button type="submit" class="btn btn-light" name="action" value="cambiarFecha" id="submit">Elegir día</button><br><br>
+                            <div class="form-row">
+                                <div class="form-group col-md-12">
+                                    <label><strong>Hora</strong></label>
+                                    <table class="table table-hover table-bordered">
+                                        <tbody>
+                                            <tr>
+                                                <?php
+                                                while($this->filaHoras = ($this->listaHoras)->fetch_assoc()) {
+                                                    $hora = $this->filaHoras['hora'];
+                                                    $reserva = new ReservaModel();
+                                                    $reserva->setFecha($this->fecha);
+                                                    $reserva->setHora($hora);
+                                                    require_once('Mappers/reservaMapper.php');
+                                                    $reservaMapper = new ReservaMapper();
+                                                    $reservasEnFecha = $reservaMapper->getNumReservasByDiaYHora($reserva);
+                                                    require_once('Mappers/pistaMapper.php');
+                                                    $pistaMapper = new PistaMapper();
+                                                    $pistasActivas = $pistaMapper->getNumPistasActivas();
+
+                                                    if($reservasEnFecha == $pistasActivas){
+                                                    ?>
+                                                        <td class="table-danger text-dark" style="text-align: -webkit-center";><?php echo date('H:i',strtotime($this->filaHoras['hora'])); ?></td>
+                                                    <?php
+                                                    }
+                                                    else{
+                                                    ?>
+                                                        <td class="table-light text-dark" style="text-align: -webkit-center";><label><input class="form-check-input" type="radio" name="inputHora" value="<?php echo $hora;?>"><?php echo date('H:i',strtotime($this->filaHoras['hora'])); ?></label></td>
+                                                    <?php
+                                                    }
+                                                
+                                                }
+                                                ?>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                            <button type="submit" class="btn btn-light" name="action" value="addPropuesta" id="submit">Proponer fecha</button>
+                        </form>
+                    </div>
+                    <div class="col-lg-2"></div>
+                </div>
+                <div class="col-lg-3"></div>
+            </div>
+
+        <?php
+        }
+    }
+    
+?>
