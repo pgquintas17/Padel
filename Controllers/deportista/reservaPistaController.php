a6a9383ff66ed87840c001738d5a5cfe7a7e151a
Paula <paulaseis@hotmail.com>
2019-12-12 12:20:25 +0100

    minor bugs

--- Controllers/deportista/reservaPistaController.php
+++ Controllers/deportista/reservaPistaController.php
@@ -60,7 +60,7 @@
 
 											$to_email_address = $_SESSION['Usuario']->getEmail();
 											$subject = 'Reserva de pista realizada.';
-											$message = '<html><head></head><body>Hola,<br>Te informamos de que has reservado pista para la fecha ' . date('d/m',strtotime($_POST["fecha"])) . ' y hora ' . date('H:i',strtotime($_POST["hora"])) . 'Tu reserva es en la pista ' . $idPista .'. Recuerda que puedes cancelarla desde tu pefil. <br><br>Un saludo,<br>Padelweb.</p></body></html>';
+											$message = '<html><head></head><body>Hola,<br>Te informamos de que has reservado pista para la fecha ' . date('d/m',strtotime($_POST["fecha"])) . ' y hora ' . date('H:i',strtotime($_POST["inputHora"])) . '.<br>Tu reserva es en la pista ' . $idPista .'.<br>Recuerda que puedes cancelarla desde tu pefil. <br><br>Un saludo,<br>Padelweb.</p></body></html>';
 											$headers  = 'MIME-Version: 1.0' . "\r\n";
 											$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 											$headers .= 'From: noreply@padelweb.com';
